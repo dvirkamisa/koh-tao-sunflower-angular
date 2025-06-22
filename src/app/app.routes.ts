959aa7bca4cbf92f0f7dfbab53ddfe04f4e88b46
dvirkamisa <dvirkamisa@gmail.com>
2025-06-22 21:25:10 +0700

    feat: add basic admin panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { JoinUsComponent } from './pages/join-us/join-us.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'join-us', component: JoinUsComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'admin', component: AdminComponent },
   { path: '**', redirectTo: '' }
-]; 
+];
